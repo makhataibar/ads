9eac28381170b983bafa2786e3da1a9b77266b70
makhataibar <makhataibar@gmail.com>
2022-07-21 16:34:18 +0600

    chore: [nx migration] update-to-export-default

--- apps/node/jest.config.ts
+++ apps/node/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
   displayName: 'node',
   preset: '../../jest.preset.js',
   globals: {
--- libs/utils/jest.config.ts
+++ libs/utils/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
   displayName: 'utils',
   preset: '../../jest.preset.js',
   globals: {
